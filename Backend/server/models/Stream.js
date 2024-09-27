44571ffb87149f449c62dfc0e9e5b94056c2b92d
MC <111571387+MahimaChavalam09@users.noreply.github.com>
2024-09-27 18:27:07 +0530

    Update Stream.js

--- Backend/server/models/Stream.js
+++ Backend/server/models/Stream.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
+
+const streamSchema = new mongoose.Schema({
+  name: String,
+  viewers: Number,
+});
+
+module.exports = mongoose.model('Stream', streamSchema);
 
